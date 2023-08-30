d7f8801be7f2a77f0940e5dd9736e55dfb322161
sobirovmurodil <murodil@gmail.com>
2023-08-30 12:27:25 +0500

    example solutions

---
+++ app.js
@@ -0,0 +1,392 @@
+// alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+// let x = +prompt('Son kiriting:')
+
+// if(x>0){
+//     console.log(x+1)
+// }else if(x<0){
+//     console.log(x-2)
+// }else{
+//     console.log(x=10)
+// }
+
+// if1, if2, if3 misollar yechimi
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let son1 = +prompt('Birinchi sonni kiriting:')
+let son2 = +prompt('Ikkinchi sonni kiriting:')
+let son3 = +prompt('Uchunchi sonni kiriting:')
+
+let musbatSon = 0
+let manfiySon = 0
+
+if(son1>0){
+    musbatSon ++
+    if(son2>0){
+        musbatSon ++
+    }else if(son2<0){
+        manfiySon ++
+    }
+    if(son3>0){
+        musbatSon ++
+    }else if(son3<0){
+        manfiySon ++
+    }
+}else if(son1<0){
+    manfiySon ++
+    if(son2>0){
+        musbatSon ++
+    }else if(son2<0){
+        manfiySon ++
+    }
+    if(son3>0){
+        musbatSon ++
+    }else if(son3<0){
+        manfiySon ++
+    }
+}
+
+console.log(`Musbat sonlar ${musbatSon} ta`)
+console.log(`Manfiy sonlar ${manfiySon} ta`)*/ //if4 va if5 misollarining yechimi
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let son1 = +prompt('Birinchi sonni kiriting:')
+let son2 = +prompt('Ikkinchi sonni kiriting:')
+
+if(son1>son2){
+    console.log(son1 + "," + son2)
+}else if(son2>son1){
+    console.log(son2 + " , " + son1)
+}*/ //if6, if7 va if8 misollarining yechimi
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('Birinchi sonni kiriting:')
+let b = +prompt('Ikkinchi sonni kiriting:')
+
+if(a>b){
+    console.log(`${a} , ${a+b}`)
+}else if(b>a){
+    console.log(a + " , " + b)
+}*/ //if9 misol yechimi
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('Birinchi sonni kiriting:')
+let b = +prompt('Ikkinchi sonni kiriting:')
+
+if(a != b){
+    console.log(a+b)
+}else if(a==b){
+    console.log(0)
+}*/ //if10 misol yechimi
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('Birinchi sonni kiriting:')
+let b = +prompt('Ikkinchi sonni kiriting:')
+
+if(a != b){
+    if(a>b){
+        console.log(a)
+    }else if(a<b){
+        console.log(b)
+    }
+}else if(a==b){
+    console.log(0)
+}*/
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let son1 = +prompt('Birinchi sonni kiriting:')
+let son2 = +prompt('Ikkinchi sonni kiriting:')
+let son3 = +prompt('Uchinchi sonni kiriting:')
+
+
+if(son1 < son2 && son3){
+    console.log(son1)
+}else if(son2 < son1 && son3){
+    console.log(son2)
+}else if(son3 < son1 && son2){
+    console.log(son3);
+}*/ //if12 misol yechimi
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let son1 = +prompt('Birinchi sonni kiriting:')
+let son2 = +prompt('Ikkinchi sonni kiriting:')
+let son3 = +prompt('Uchinchi sonni kiriting:')
+
+
+if(son1 > son2 &&son1 < son3){
+    console.log(son1)
+}else if(son2 > son1 && son2 < son3){
+    console.log(son2)
+}else if(son3 > son1 && son3 < son2){
+    console.log(son3);
+}*/ //if13 misollar yechimi 
+
+// BEGIN1
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let R = +prompt('Doiraning radiusini  kiriting:')
+
+let pi = Math.PI
+
+let L = (2*pi*R).toFixed(1)
+let S = (pi*(R**2)).toFixed(1)
+
+console.log(L, S)*/
+
+// BEGIN2
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let son1 = +prompt('Musbat son kiriting:')
+let son2 = +prompt('Musbat son kiriting:')
+
+let G = Math.sqrt(son1 * son2)
+
+console.log(G)*/
+
+// BEGIN3
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let x = +prompt('X ni kiriting:')
+
+let y = 3 * (x**6) - 6 * (x**2) - 7
+
+console.log(y)*/
+
+// integer1
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let x = +prompt('Ikki xonali son kiriting:')
+
+let birinchiRaqam = Math.trunc(x / 10)
+let ikkinchiRaqam = x % 10
+
+console.log(birinchiRaqam + ikkinchiRaqam)*/
+
+// Integer 2
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let x = +prompt('Ikki xonali son kiriting:')
+
+let birinchiRaqam = Math.trunc(x / 10)
+let ikkinchiRaqam = x % 10
+
+console.log(`${ikkinchiRaqam}${birinchiRaqam}`)*/
+
+// Integer 3
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let x = +prompt('Uch xonali son kiriting:')
+
+console.log(Math.trunc(x/100))*/
+
+// Integer 4
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let x = +prompt('To`rt xonali son kiriting:')
+
+let ikkinchiRaqam = Math.trunc(x/100) % 10
+
+console.log(ikkinchiRaqam)*/
+
+// UYGA VAZIFA
+
+// BEGIN1
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('A tomonni kiriting:')
+let b = +prompt('B tomonni kiriting:')
+
+let p = 2 * (a+b)
+let s = a*b
+
+console.log(`Perimetr = ${p}, Yuza = ${s}`)*/
+
+// BEGIN 2
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let d = +prompt('Diametrni kiriting:')
+
+let pi = Math.PI.toFixed(2)
+let l = Math.trunc(pi * d)  
+let r = d / 2
+let s = Math.trunc(pi * (r/2))
+
+console.log(`Uzunligi = ${l}, Yuzasi = ${s}`)*/
+
+// BEGIN 3
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('Kubning yon tomonini kiriting:')
+
+let v = a **3
+let s = 6 * (a**2)
+
+console.log(`Hajmi = ${v}, To'la sirti = ${s}`)*/
+
+// BEGIN 4
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('A tomonni kiriting:')
+let b = +prompt('B tomonni kiriting:')
+let c = +prompt('C tomonni kiriting:')
+
+let v = a  * b * c
+let s = 2 * (a * b + b * c + a * c)
+
+console.log(`Hajmi ${v}, To'la sirti ${s}`)*/ 
+
+// BEGIN 5
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('Birinchi sonni kiriting:')
+let b = +prompt('Ikkinchi sonni kiriting:')
+
+let m = (a+b)/2
+console.log(m)*/
+
+// BEGIN 6
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('Birinchi tomonni kiriting:')
+let b = +prompt('Ikkinchi tomonni kiriting:')
+
+let c = Math.trunc(Math.sqrt((a**2) + (b**2)))
+let p = a+b+c
+
+console.log(`Gipotenuzasi= ${c}, Perimetri ${p}`)*/
+
+// BEGIN 7
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let r1 = +prompt('Birinchi aylana radiusini kiriting:')
+let r2 = +prompt('Ikkinchi aylana radiusini kiriting:')
+
+let pi = Math.PI
+let s1 = Math.trunc(pi * r1)
+let s2 = Math.trunc(pi * r2)
+let s = Math.trunc(pi * ((r1**2)-(r2**2)))
+
+if(r1>r2){
+    console.log(`S1 = ${s1}, S2 = ${s2}, S = ${s}`)
+}else{
+    console.log("R1 R2 dan kichik bo'lmaydi. Qayta qiymat kiriting.")
+}*/ 
+
+// BEGIN 8
+
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let l = +prompt('Aylananing uzunligini kiriting:')
+
+let pi = Math.PI
+let r = Math.trunc(2 * pi * l)
+let s = Math.trunc(pi * (r**2))
+console.log(`Aylananing radiusi ${r}, Yuzasi ${s}`)*/
+
+// BEGIN 9
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let x1 = +prompt(' X1ni kiriting:')
+let y1 = +prompt(' Y1ni kiriting:')
+let x2 = +prompt(' X2ni kiriting:')
+let y2 = +prompt(' Y2ni kiriting:')
+
+let l = Math.trunc(Math.sqrt((x1-x2)**2 + (y1-y2)**2))
+console.log(`Masofa ${l}`)*/
+
+// BEGIN 10
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt(' Ani kiriting:')
+let b = +prompt(' Bni kiriting:')
+let c = +prompt(' Cni kiriting:')
+
+a == b
+b == c 
+c == a 
+console.log(a, b, c)*/
+
+// BEGIN 11
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let x = +prompt(' Xni kiriting:')
+
+let  y = 4*(x-3)**6-7*(x-3)**3+2
+console.log(y)*/
+
+// BEGIN 12
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt(' Ani kiriting:')
+
+let daraja3 = a**3
+let daraja6 = a**6
+let daraja9 = a**9
+let daraja15 = a**15
+
+console.log(daraja3, daraja6, daraja9, daraja15)*/
+
+// BEGIN 13
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let Tf = +prompt(' Temperaturani kiriting:')
+let Tc = (Tf-32)*5/9
+console.log(Tc)*/
+
+// Integer 1
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('Uch xonali son kiriting:')
+
+let birlik = a%10
+let onlik = ((a%100)-((a%100) % 10 ))/10
+console.log(birlik, onlik)*/
+
+// INTEGER 2
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('Uch xonali son kiriting:')
+
+let birlik = a%10
+let onlik = ((a%100)-((a%100) % 10 ))/10
+let yuzlik = (a-(a%100))/100
+console.log(yuzlik + onlik + birlik)*/
+
+// INTEGER 5
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('Uch xonali son kiriting:')
+
+let birlik = a%10
+let onlik = ((a%100)-((a%100) % 10 ))/10
+let yuzlik = (a-(a%100))/100
+console.log(birlik, onlik, yuzlik)*/
+
+// INTEGER 4
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('Uch xonali son kiriting:')
+
+let birlik = a%10
+let onlik = ((a%100)-((a%100) % 10 ))/10
+let yuzlik = (a-(a%100))/100
+console.log(onlik, yuzlik, birlik)*/
+
+// INTEGER 5
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let a = +prompt('Uch xonali son kiriting:')
+
+let birlik = a%10
+let onlik = ((a%100)-((a%100) % 10 ))/10
+let yuzlik = (a-(a%100))/100
+console.log(yuzlik, birlik, onlik)*/
+
+// INTEGER 7
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let sekund = +prompt('Sekundni kiriting:')
+
+let minut = Math.trunc(sekund/60)
+console.log(minut)*/
+
+// INTEGER 8
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let sekund = +prompt('Sekundni kiriting:')
+
+let soat = Math.trunc(sekund/3600)
+console.log(soat)*/
+
+// INTEGER 10
+/*alert("Assalomu alaykum, mening dasturimga hush kelibsiz")
+let sekund = +prompt('Sekundni kiriting:')
+
+let soat = Math.trunc(sekund/60/60)
+console.log(soat, "soat")
+let minut = Math.trunc((sekund-soat*3600)/60)
+console.log(minut, "minut")
+let soniya = sekund - soat * 3600 - minut * 60
+console.log(soniya, "sekund") */
